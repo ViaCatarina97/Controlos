dcb4f8d4695a3226cf0cb4f6373b0c19528aa946
ViaCatarina97 <viacatarina@mcdimperial.com>
2025-12-23 14:44:29 +0000

    Update supabaseClient.ts

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Podes encontrar estes valores em Supabase > Settings > API
-const supabaseUrl = https://yjfidxhvwblhyxdxwhuu.supabase.co;
-const supabaseKey = sb_publishable_pO8tjSbdX6H5HDcfuFAQ7Q_AlS2UAhq;
+const supabaseUrl = 'https://yjfidxhvwblhyxdxwhuu.supabase.co';
+const supabaseKey = 'sb_publishable_pO8tjSbdX6H5HDcfuFAQ7Q_AlS2UAhq';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
