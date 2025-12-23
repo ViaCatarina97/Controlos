b3f6572c26b288859a303993eb5af34d795a7427
ViaCatarina97 <viacatarina@mcdimperial.com>
2025-12-23 17:26:44 +0000

    Update supabaseClient.ts

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,11 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-// No Vite, usamos import.meta.env
-const supabaseUrl = import.meta.env.https://yjfidxhvwblhyxdxwhuu.supabase.co;
-const supabaseKey = import.meta.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlqZmlkeGh2d2JsaHl4ZHh3aHV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY0OTMyMTQsImV4cCI6MjA4MjA2OTIxNH0.zOWj65-yWGOZkif38py8MPw8jiegS7R4efGQP67DL0E;
-
-if (!supabaseUrl || !supabaseKey) {
-  console.error("ERRO: As chaves do Supabase não foram encontradas!");
-}
+// O código fica literalmente assim:
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
